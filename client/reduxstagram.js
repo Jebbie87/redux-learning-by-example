12301c6c38749d767f0bae4c9f129cc89217e679
Jebbie87 <jeffreycj.chang@gmail.com>
2017-03-07 16:08:58 -0500

    Finished Lesson #5

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -2,8 +2,24 @@ import React from 'react'
 
 import { render } from 'react-dom'
 
+// import CSS
 import css from './styles/style.styl'
 
+// import components
 import Main from './components/Main'
+import PhotoGrid from './components/PhotoGrid'
+import Single from './components/Single'
 
-render(<Main/>, document.getElementById('root'))
+// import react router apps
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path='/' component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'))
