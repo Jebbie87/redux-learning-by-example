b22d321bfec26dfafa9c373e4b21c7689adbe668
Jebbie87 <jeffreycj.chang@gmail.com>
2017-03-09 16:07:42 -0500

    Finished lesson #16

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,10 +1,34 @@
-function comments(state = [], action) {
+function postComments(state = [], action) {
   switch(action.type) {
     case 'ADD_COMMENT':
-      console.log('add comment')
+      // return existing state with new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }]
+    case 'REMOVE_COMMENT':
+      // we need to return the new state without the deleted comment
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index + 1)
+      ]
     default:
       return state
   }
+  return state
 }
+
+function comments(state = [], action) {
+  if (typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state,
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
+  return state
+}
+
 
 export default comments
